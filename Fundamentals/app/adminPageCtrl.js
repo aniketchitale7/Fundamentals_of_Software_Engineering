49bb335b6962fc318a9a2996a5d16e62ae4f1160
MaheshSasidharan <mahesh.sasidharan.1990@gmail.com>
2016-03-21 11:16:46 -0500

    latest changes

--- Fundamentals/app/adminPageCtrl.js
+++ Fundamentals/app/adminPageCtrl.js
@@ -3,6 +3,7 @@
  * Created by Aniket
  */
 app.controller("adminPageCtrl", function($scope, $rootScope, $http, $location, $cookieStore){
+   alert("Hello");
     $scope.user_name = $rootScope.session.user_name;
     $scope.access = $rootScope.session.access;
 
